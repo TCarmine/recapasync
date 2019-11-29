d11236b4521a822670154e35d09673409faefc9e
TCarmine <kappa85t@hotmail.it>
2019-11-29 22:09:46 +0100

    callback hell

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 console.log('Before');
 
 getUser(1,(user)=>{
-    console.log('User', user);
-});
-
+    getRepository(user.gitHubUsername,(repos)=>{
+       getCommits('Carmine',)
+    });
+}); 
 console.log('After');
+
+function getCommits(getUser, callback){
+    setTimeout(()=>{
+            let commits = ['ahagsd','ahshgegeh','jeheee'];
+            callback({commits});
+            return;
+    },2000); 
+};
 
 function getUser(id, callback){
     setTimeout(()=>{
         console.log('Reading user info from DB...');
         callback({id:id, gitHubUsername:'carmine'});
         return;
-      },2000);
+    },2000);
+};
+
+function getRepository(username,callback){
+   setTimeout(()=>{
+       let repos= ['repo1','repo2','repo3'];
+       //here the array need to be wrapped otherwise callback is seen as a function
+       // and not a callback
+       callback({repos});
+       return;
+   },2000);
 }
