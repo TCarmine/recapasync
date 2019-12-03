bf74961596ea6adb471451ff70c746c75191c6d8
TCarmine <kappa85t@hotmail.it>
2019-12-03 17:58:06 +0100

    basic Promise for retrive file data

--- Stackoverflow.js
+++ Stackoverflow.js
@@ -1,39 +1,61 @@
-var movie_ids = [
-    474350, 522938, 540901,
-    384018, 419704, 420818,
-    521029, 301528, 484641,
-    515195, 466272, 441282,
-    423204, 533642, 530385,
-    568012, 535544, 456740,
-    523077, 531309
-]
+// var movie_ids = [
+//     474350, 522938, 540901,
+//     384018, 419704, 420818,
+//     521029, 301528, 484641,
+//     515195, 466272, 441282,
+//     423204, 533642, 530385,
+//     568012, 535544, 456740,
+//     523077, 531309
+// ]
 
-async function insert_ids(movie_ids) {
+// async function insert_ids(movie_ids) {
 
-    var promiseArray = [];
+//     var promiseArray = [];
 
-    for (var i = 1; i <= movie_ids.length; i++) {
-        promiseArray.push(new Promise((resolve, reject) => {
-            var url1 = "FIRST PART";
-            var url2 = "SECOND PART";
+//     for (var i = 1; i <= movie_ids.length; i++) {
+//         promiseArray.push(new Promise((resolve, reject) => {
+//             var url1 = "FIRST PART";
+//             var url2 = "SECOND PART";
 
-            request(url1 + movie_ids[i] + url2, function (err, res, body) {
-                    if (!err & res.statusCode == 200) {
-                        var data = JSON.parse(body);
-                        resolve(data.imdb_id);
-                        console.log(data.imdb_id); // This prints the correct ids 
-                    } else {
-                        console.log(err); //> Prints "null"
-                    }
-                });
-        }));
-    };
+//             request(url1 + movie_ids[i] + url2, function (err, res, body) {
+//                     if (!err & res.statusCode == 200) {
+//                         var data = JSON.parse(body);
+//                         resolve(data.imdb_id);
+//                         console.log(data.imdb_id); // This prints the correct ids 
+//                     } else {
+//                         console.log(err); //> Prints "null"
+//                     }
+//                 });
+//         }));
+//     };
 
 
-    await Promise.all(promiseArray).then(function(result) {
-        console.log(result);
+//     await Promise.all(promiseArray).then(function(result) {
+//         console.log(result);
+//     });
+
+// }
+
+// insert_ids(movie_ids);
+
+const fs = require("fs");
+
+function readLocal(filename, encoding) {
+  const readP = new Promise((resolve, reject)=>{
+    fs.readFile(filename, encoding,(err, data) => {
+          if(!err){
+            console.log(`Promise :${readP} is been resolved`);
+            resolve(data);
+          }else{
+            console.log(`Promise :${readP} is been rejected`);
+            reject(err);
+          }
     });
-
+  })
+  return readP;
 }
 
-insert_ids(movie_ids);
+// /home/ca/Desktop/nodejs/recapasync/
+readLocal("./hello.txt", "utf-8")
+          .then( data => console.log(data))
+          .catch( err => console.log('Err: ', err.message));
